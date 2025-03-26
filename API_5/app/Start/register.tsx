7c12f2490d1f9c1a247cc49c913d24ad38a3ab94
Silmara Bittencourt <silmarabittencourt13@gmail.com>
2025-03-26 18:24:25 -0300

    Fazendo tela de registro de adm

--- API_5/app/Start/register.tsx
+++ API_5/app/Start/register.tsx
@@ -1,29 +1,114 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+import * as Font from 'expo-font'; 
 
-export default function App() {
+const Login = () => {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync({
+          Roboto: require('../../assets/fonts/Roboto-Regular.ttf'),
+      });
+      setFontLoaded(true);
+    };
+
+    loadFonts();
+  }, []);
+
+  if (!fontLoaded) {
+    return <Text>Carregando fontes...</Text>;
+  }
+
   return (
     <View style={styles.container}>
+      <StatusBar style="auto" />
+      
       <Image
-        source={require('../assets/project_images/logotipo.png')}
+        source={require('../../assets/project_images/logotipo.png')}
         style={styles.image}
         resizeMode="contain"
       />
-      <StatusBar style="auto" />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome"
+        placeholderTextColor="#B8B8B8" 
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+        placeholderTextColor="#B8B8B8" 
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        secureTextEntry
+        placeholderTextColor="#B8B8B8" 
+      />
+
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Cadastrar</Text>
+      </TouchableOpacity>
+
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2E2E2E',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#2E2E2E',
+    padding: 24,
+  },
+  input: {
+    padding: 24,
+    fontSize: 24,
+    width: '100%',
+    height: 40,
+    borderColor: '#F5F5F5',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingLeft: 30,
+    borderRadius: 5,
+    color: '#111',
+    backgroundColor: '#F5F5F5',
+    fontFamily: 'Roboto',
   },
   image: {
-    flex: 2,
-    width: 250,  
-    height: 'auto', 
+    width: 250,
+    height: 150,
+  },
+  button: {
+    borderWidth: 1, 
+    borderColor: '#F5F5F5',
+    backgroundColor: '#282828',  
+    borderRadius: 10,  
+    paddingVertical: 12, 
+    paddingHorizontal: 50,  
+    marginTop: 20,  
+  },
+  buttonText: {
+    color: '#F5F5F5',  
+    fontSize: 24,
+    fontFamily: 'Roboto',
+    textAlign: 'center',
+  },
+  registerContainer: {
+    marginTop: 20,  
+  },
+  registerText: {
+    color: '#F5F5F5',
+    fontSize: 16,
+  },
+  linkText: {
+    color: '#B8B8B8', 
   },
 });
+
+export default Login;
