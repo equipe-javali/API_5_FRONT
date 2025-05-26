c6c796db0dc971fc9a13f0c239746e1b45828d56
BrunoSerpa <serpabruno2005@gmail.com>
2025-05-26 01:59:13 -0300

    Task-169: Criei o tipo alternativo de BaseScreen

--- API_5/components/BaseScreen/index.tsx
+++ API_5/components/BaseScreen/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { ReactNode, useEffect, useState, version } from "react";
 import { SafeAreaView, ScrollView, View } from "react-native";
 import styles from "./style";
 import { StatusBar } from "react-native";
@@ -6,11 +6,12 @@ import * as Font from 'expo-font';
 import Loading from "../Loading";
 
 interface BaseScreenProps {
+    alternative?: boolean;
     children: ReactNode;
     header?: ReactNode;
 };
 
-export default function BaseScreen({ children, header }: BaseScreenProps) {
+export default function BaseScreen({ alternative, children, header }: BaseScreenProps) {
     const [Loaded, setLoaded] = useState(false);
 
     useEffect(() => {
@@ -23,18 +24,27 @@ export default function BaseScreen({ children, header }: BaseScreenProps) {
         loadFonts();
     }, []);
 
+
     return (
         <SafeAreaView style={styles.mainContainer}>
             <StatusBar hidden />
-            {header}            
-            <ScrollView style={styles.container}>
-                {Loaded ?
-                    <View style={styles.divContainer}>
-                        {children}
-                    </View> :
-                    <Loading textLoading="Carregando fontes" />
-                }
-            </ScrollView>
+            {header}
+            {alternative ?
+                <View style={styles.container}>
+                    {Loaded ?
+                        children :
+                        <Loading textLoading="Carregando fontes" />
+                    }
+                </View> :
+                <ScrollView style={styles.container}>
+                    {Loaded ?
+                        <View style={styles.divContainer}>
+                            {children}
+                        </View> :
+                        <Loading textLoading="Carregando fontes" />
+                    }
+                </ScrollView>
+            }
         </SafeAreaView>
     );
 };
